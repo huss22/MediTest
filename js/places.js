4f4d323ef55ac313d6f02955a15fc4ae48c638d1
Hasnain Afzal <huss22@users.noreply.github.com>
2014-11-01 18:05:44 +1100

    Update places.js

--- js/places.js
+++ js/places.js
@@ -1,289 +1,89 @@
-<!doctype html>
-<html ng-app="ui.bootstrap.demo">
-  <head>
-    
-    <link href="css/bootstrap-3.2.0-dist/css/bootstrap.css" rel="stylesheet">
-    <link href="css/main.css" rel="stylesheet">
-    <link href="fonts/font-awesome/css/font-awesome.min.css" rel="stylesheet">
-    <link href="fonts/weather-icons-master/css/weather-icons.min.css" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="components/sidebars/css/normalize.css" />
-    <link rel="stylesheet" type="text/css" href="components/sidebars/css/demo.css" />
-   
-    <link rel="stylesheet" type="text/css" href="components/sidebars/css/component.css" />
-    
-    
-  </head>
-  <body>
+var map;
+var service;
+var infowindow;
+var x = document.getElementById("pharmacyresult");
 
-<!--Application Header-->
+function initialize(position) {
+  getLocation();
+  var pyrmont = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
 
+  map = new google.maps.Map(document.getElementById('map'), {
+      center: pyrmont,
+      zoom: 15
+    });
 
+  var request = {
+    location: pyrmont,
+    radius: '500',
+    query: 'pharmacy'
+  };
 
-<!--End of Header-->
+  var request2 = {
+    location: pyrmont,
+    radius: '500',
+    query: 'GP'
+  };
 
-<!--Main application content starts here-->
-<div id="st-container" class="st-container">
-<div class="st-pusher">
-<nav class="st-menu st-effect-6" id="menu-6">
-          <h2 class="icon-user">Ali Afzal</h2>
-          <ul>
-            <li><a class="icon icon-data" href="#">DOB: not selected</a></li>
-            <li><a class="icon icon-location" href="#">Allergy: none selected</a></li>
-            <li><a class="icon icon-study" href="#">Emergency Contact: not selected</a></li>
-            <li><a class="icon icon-photo" href="#">My GP: not selected</a></li>
-            <li><a class="icon icon-wallet" href="#">My Pharmacy: not selected</a></li>
-          </ul>
-        </nav>
+  var request3 = {
+    location: pyrmont,
+    radius: '500',
+    query: 'Hospital'
+  };
 
-<div class="st-content"><!-- this is the wrapper for the content -->
-          <div class="st-content-inner"><!-- extra div for emulating position:fixed of the menu -->
-            <!-- Top Navigation -->
-            <div class="appHead">
-              
-              <h3 class="text-center"><i class="wi wi-thermometer"></i> Application Name</h3>
-            </div>
-            
-            <div class="main clearfix">
-              <div id="st-trigger-effects" class="column">
-                
-                <div class="container">
+  service = new google.maps.places.PlacesService(map);
+  service.textSearch(request, callback);
+  service.textSearch(request2, callback2);
+  service.textSearch(request3, callback3);
+}
 
-    <div class="appContent">
+function callback(results, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+      var place = results[0];
+      createMarker(results[0]);
+  }
+}
 
-      <div class="weekHead">
-        <h4 class="text-center">Week no.1: 1-Oct-14 to 7-Oct-14</h4>
-      </div>
+function callback2(results, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+      var place = results[0];
+      createMarker2(results[0]);
+  }
+}
 
-        <div class="row">
-          <div class="container timeRow">
-            <div class="blank"> <h5></h5></div>
-            <div class="timeName" style="background-color:#a38acc;"><i class="wi wi-horizon-alt"></i> Morn</div>
-            <div class="timeName" style="background-color:yellow; color: #00AEEF;"><i class="wi wi-day-sunny"></i> Noon</div>
-            <div class="timeName" style="background-color:#f0ad4e;"><i class="wi wi-horizon"></i> Eve</div>
-            <div class="timeName" style="background-color:#000;"><i class="wi wi-night-clear"></i>Bed</div>
-          </div>
-        </div>
+function callback3(results, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+      var place = results[0];
+      createMarker3(results[0]);
+  }
+}
 
-        <div class="row">
-          <div class="container">
-            <div class="dayName">Mon</div>
-            <div class="dayAdd">
-             <div ng-controller="ModalDemoCtrl">
-                <script type="text/ng-template" id="myModalContent.html">
-                  <div class="modal-header">
-                    <h5 class="modal-title text-center">My Daily Medication Tracker</h5>
-                  </div>
-                  
-                  <div class="modal-body">
-                    <div class="bs-callout bs-callout-app">
-                      <h6>Medicine Name</h6>
-                        <div class="appinput">
-                          <div class="input-group">
-                            <input type="text" class="form-control">
-                            <span class="input-group-btn">
-                              <button class="btn btn-default btn-default-app" type="button">Add!</button>
-                            </span>
-                          </div><!-- /input-group -->
-                        </div><!-- /.col-lg-6 -->
+function createMarker(place) {
+  $('#pharmacyresult').empty();
+   $('#pharmacyresult').append('<br><b>Closest Pharmacy: </b>' + place.name + ", located at " + place.formatted_address);
+   $('#pharmacyresult').append(place.opening_hours);
+   console.log(place);
+}
 
-                      </div>
+function createMarker2(place) {
+  $('#gpresult').empty();
+   $('#gpresult').append('<br><b>Closest GP: </b>' + place.name + ", located at " + place.formatted_address);
+   $('#gpresult').append(place.opening_hours);
+   console.log(place);
+}
 
-                        <div class="panel panel-info">
-                          <div class="panel-heading">Medication Name</div>
-                            <div class="panel-body">
-                              <div class="alert alert-info">
-                                  <div class="media">
-                                    <span class="glyphicon glyphicon-info-sign pull-left"></span> 
-                                    <div class="media-body">
-                                      <h3 class="media-heading">Please select your medication!</h3>
-                                      <label>Type in the part of the medication name.</label> 
-                                      <div class="input-group">
-                                        <input type="text" class="form-control" placeholder="Amoxill"> 
-                                        <span class="input-group-btn"> 
-                                          <button class="btn btn-primary" type="button">Add!</button> 
-                                        </span>
-                                      </div>
-                                      <h6 class="help-block text-info">Please select as many medication as required</h6>
-                                    </div>
-                                  </div>
+function createMarker3(place) {
+  $('#hospitalresult').empty();
+   $('#hospitalresult').append('<br><b>Closest Hospital: </b>' + place.name + ", located at " + place.formatted_address);
+   $('#hospitalresult').append(place.opening_hours);
+   console.log(place);
+}
 
-                              </div><!-- /.col-lg-6 -->
-                              <div class="callout callout-success">
-      <h4>Callout heading</h4>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda, alias, in accusantium totam adipisci vel et suscipit quidem libero pariatur minus ratione quo doloremque error at nemo incidunt dicta quia?</p>
-    </div>
-                            </div>
-                          </div>
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(initialize);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
 
-                           <div class="bs-callout bs-callout-app">
-                            <h6>Set a Reminder</h6>
-                            <div class="container">
-                              <div ng-controller="TimepickerDemoCtrl">
-                                <timepicker ng-model="mytime" ng-change="changed()" hour-step="hstep" minute-step="mstep" show-meridian="ismeridian"></timepicker>
-                              </div>
-                            </div>
-                        </div>
-                         <div class="panel panel-default">
-                          <div class="panel-heading"><h3 class="panel-title">Form inside Alert</h3></div>
-                          <div class="panel-body">
-                              <div class="alert alert-info">
-                                  <div class="media">
-                                    <span class="glyphicon glyphicon-info-sign pull-left"></span> 
-                                    <div class="media-body">
-                                      <h3 class="media-heading">Please select your medication!</h3>
-                                      <label>Type in the part of the medication name.</label> 
-                                      <div class="input-group">
-                                        <input type="text" class="form-control" placeholder="Amoxill"> 
-                                        <span class="input-group-btn"> 
-                                          <button class="btn btn-primary" type="button">Submit</button> 
-                                        </span>
-                                      </div>
-                                      <h6 class="help-block text-info">Please select as many medication as required</h6>
-                                    </div>
-                                  </div>
-                              </div>
-                          </div><!--panel-body-->
-                      </div><!--panel-->
-
-                  </div>
-                  
-                  <div class="modal-footer">
-                    <button class="btn btn-primary" ng-click="ok()">OK</button>
-                    <button class="btn btn-warning" ng-click="cancel()">Cancel</button>
-                  </div>
-                </script>
-                <i class="icon-plus" ng-click="open()"></i>
-            </div>
-          </div>
-            <div class="dayAdd">
-            <i class="icon-plus" data-toggle="modal" data-target="#myModal"></i>
-              <!-- Modal -->
-              <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-                <div class="modal-dialog">
-                  <div class="modal-content">
-                    <div class="modal-header">
-                      <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-                      <h4 class="modal-title" id="myModalLabel">My Medication Tracker</h4>
-                    </div>
-                    <div class="modal-body">
-                      
-
-                    </div>
-                    <div class="modal-footer">
-                      <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                      <button type="button" class="btn btn-primary">Save changes</button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              
-
-            </div>
-            <div class="dayAdd"><i class="icon-plus"></i></div>
-            <div class="dayAdd"><i class="icon-plus"></i></div>
-          </div>
-        </div>   
-
-        <div class="row">
-          <div class="container">
-            <div class="dayName">Tue</div>
-            <div class="dayAdd"><i class="icon-plus"></i></div>
-            <div class="dayAdd"><i class="icon-plus"></i></div>
-            <div class="dayAdd"><i class="icon-plus"></i></div>
-            <div class="dayAdd"><i class="icon-plus"></i></div>
-          </div>
-        </div>  
-
-        <div class="row">
-          <div class="container">
-            <div class="dayName">Wed</div>
-            <div class="dayAdd"><i class="icon-plus"></i></div>
-            <div class="dayAdd"><i class="icon-plus"></i></div>
-            <div class="dayAdd"><i class="icon-plus"></i></div>
-            <div class="dayAdd"><i class="icon-plus"></i></div>
-          </div>
-        </div>  
-
-        <div class="row">
-          <div class="container">
-            <div class="dayName">Thu</div>
-            <div class="dayAdd"><i class="icon-plus"></i></div>
-            <div class="dayAdd"><i class="icon-plus"></i></div>
-            <div class="dayAdd"><i class="icon-plus"></i></div>
-            <div class="dayAdd"><i class="icon-plus"></i></div>
-          </div>
-        </div>  
-
-        <div class="row">
-          <div class="container">
-            <div class="dayName">Fri</div>
-            <div class="dayAdd"><i class="icon-plus"></i></div>
-            <div class="dayAdd"><i class="icon-plus"></i></div>
-            <div class="dayAdd"><i class="icon-plus"></i></div>
-            <div class="dayAdd"><i class="icon-plus"></i></div>
-          </div>
-        </div>  
-
-        <div class="row">
-          <div class="container">
-            <div class="dayName">Sat</div>
-            <div class="dayAdd"><i class="icon-plus"></i></div>
-            <div class="dayAdd"><i class="icon-plus"></i></div>
-            <div class="dayAdd"><i class="icon-plus"></i></div>
-            <div class="dayAdd"><i class="icon-plus"></i></div>
-          </div>
-        </div>  
-
-        <div class="row">
-          <div class="container">
-            <div class="dayName">Sun</div>
-            <div class="dayAdd"><i class="icon-plus"></i></div>
-            <div class="dayAdd"><i class="icon-plus"></i></div>
-            <div class="dayAdd"><i class="icon-plus"></i></div>
-            <div class="dayAdd"><i class="icon-plus"></i></div>
-          </div>
-        </div>   
-
-    </div>
-
-              </div>
-              <br/>
-             <button class="pull-left" data-effect="st-effect-6"><i class="icon-user">My Profile</i></button>
-             <button class="pull-left" ><i class="icon-user">closest pharmacy</i></button>
-            <div id="map"></div>
-            <div id="pharmacyresult"></div>
-            <div id="gpresult"></div>
-            <div id="hospitalresult"></div>
-            
-
-            </div><!-- /main -->
-          </div><!-- /st-content-inner -->
-        </div><!-- /st-content -->
-</div><!-- /st-pusher -->
-</div>
-</div>
-
-    
-    
-    <script src="js/jquery.js"></script>
-    <script src="js/main.js"></script>
-    <script src="js/collapse.js"></script>
-    <script src="js/modal.js"></script>
-    <script src="js/alert.js"></script>
-    <script src="js/places.js"></script>
-
-    <script src="components/sidebars/js/classie.js"></script>
-    <script src="components/sidebars/js/sidebarEffects.js"></script>
-    <script src="components/tabs/js/cbpFWTabs.js"></script>
-    <script>
-      (function() {
-
-        [].slice.call( document.querySelectorAll( '.tabs' ) ).forEach( function( el ) {
-          new CBPFWTabs( el );
-        });
-
-      })();
-    </script>
-<!--Finish loading scripts here-->
-  </body>
-</html>
+google.maps.event.addDomListener(window, 'load', getLocation);
